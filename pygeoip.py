07bdc35ea4d4a0eaa8047e016493db66a0aec84e
cashlessclay <cashless.clay@outlook.com>
2022-04-14 23:52:40 +0200

    commit some changes

--- pygeoip.py
+++ pygeoip.py
@@ -1,3 +1,7 @@
+import os
+import socket
+import platform
+import sys
 import urllib2
 import json
 from urllib2 import urlopen
@@ -5,12 +9,22 @@ from urllib2 import urlopen
 url = 'http://ipinfo.io/json'
 response = urlopen(url)
 data = json.load(response)
-
+username=os.getlogin()
+nameof=os.environ.get('USERNAME')
+hostname=socket.gethostname()
+fqdn=socket.getfqdn()
+osname=os.name
+platform=platform.system()
+#architecture=platform.architecture()
+#machine=platform.machine()
+#release=platform.release
+sysplatform=sys.platform
 IP=data['ip']
 org=data['org']
 city = data['city']
 country=data['country']
 region=data['region']
+print ('HELLO {0} - {1} - {2} - {3} - {4} - {5} - {6}'.format(username,nameof,hostname,fqdn,osname,platform,sysplatform))
 print ('\n][-=================================')
 print ('][-Your IP detail')
 print ('IP : {4} \nRegion : {1} \nCountry : {2} \nCity : {3} \nOrg : {0}'.format(org,region,country,city,IP))
